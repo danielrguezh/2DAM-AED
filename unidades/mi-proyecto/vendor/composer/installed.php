c17a4476d6e93e692a39a34666daab455ce2706e
mackstm <thelewyntm@gmail.com>
2025-10-01 18:10:56 +0100

    proyecto

---
+++ unidades/mi-proyecto/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '76426a78a18ee8d0bb8f1176f1485c5eff8a8ea6',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '76426a78a18ee8d0bb8f1176f1485c5eff8a8ea6',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
